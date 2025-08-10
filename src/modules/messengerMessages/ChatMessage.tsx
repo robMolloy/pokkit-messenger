e4adfe86f0aa9d10cae5054a2bbf34f2084b2973
robmolloy <robmolloy@hotmail.co.uk>
2025-08-10 22:45:22 +0100

    working RecipientScreen with chatMessage

---
+++ src/modules/messengerMessages/ChatMessage.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import { TUser } from "@/modules/users/dbUsersUtils";
+
+export const ChatMessage = (p: {
+  message: React.ReactNode;
+  user: TUser;
+  isOwnMessage: boolean;
+}) => {
+  return (
+    <div className="flex max-w-[70%] flex-col gap-1">
+      <div className="mb-1 text-xs font-medium">{p.user.name}</div>
+      <div className="flex">
+        <div
+          className={cn(
+            "rounded-lg px-4 py-2",
+            p.isOwnMessage
+              ? "bg-blue-500 text-white"
+              : "bg-secondary text-gray-900 dark:bg-gray-700 dark:text-gray-100",
+          )}
+        >
+          <div className="whitespace-pre-wrap break-words text-sm">{p.message}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/modules/messengerMessages/screens/RecipientScreen.tsx
+++ src/modules/messengerMessages/screens/RecipientScreen.tsx
@@ -1,12 +1,25 @@
 import { MainFixedLayout, Scroll } from "@/components/layout/Layout";
 import { H1 } from "@/components/ui/defaultComponents";
-import { Textarea } from "@/components/ui/textarea";
-import { useMessengerMessageRecordsStore } from "../messengerMessagesStore";
+import { pb } from "@/config/pocketbaseConfig";
 import { useUsersStore } from "@/modules/users/usersStore";
+import { useCurrentUserStore } from "@/stores/authDataStore";
+import { useState } from "react";
+import { createMessengerMessageRecord } from "../dbMessengerMessagesUtils";
+import { useMessengerMessageRecordsStore } from "../messengerMessagesStore";
+import { MessengerMessageTextarea } from "../MessengerMessageTextarea";
+import { ChatMessage } from "../ChatMessage";
 
 const useRecipientWithMessengerMessages = (p: { recipientId: string }) => {
+  const currentUserStore = useCurrentUserStore();
   const usersStore = useUsersStore();
   const messengerMessagesStore = useMessengerMessageRecordsStore();
+
+  if (currentUserStore.data.authStatus === "loading") return { status: "loading" } as const;
+  if (currentUserStore.data.authStatus === "loggedOut") return { status: "error" } as const;
+  // This should never be hit
+  if (currentUserStore.data.authStatus !== "loggedIn") return { status: "error" } as const;
+
+  const currentUser = currentUserStore.data.user;
 
   if (usersStore.data === undefined) return { status: "loading" } as const;
   if (usersStore.data === null) return { status: "error" } as const;
@@ -21,12 +34,16 @@ const useRecipientWithMessengerMessages = (p: { recipientId: string }) => {
     (x) => x.recipientId === p.recipientId || x.senderId === p.recipientId,
   );
 
-  return { status: "success", data: { recipient, messages } } as const;
+  return { status: "success", data: { currentUser, recipient, messages } } as const;
 };
 export const RecipientScreen = (p: { recipientId: string }) => {
   const recipientWithMessengerMessages = useRecipientWithMessengerMessages({
     recipientId: p.recipientId,
   });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [text, setText] = useState("");
   return (
     <MainFixedLayout>
       <H1>Scrolling page with fixed items</H1>
@@ -35,17 +52,44 @@ export const RecipientScreen = (p: { recipientId: string }) => {
         if (recipientWithMessengerMessages.status === "loading") return <div>loading</div>;
         if (recipientWithMessengerMessages.status === "error") return <div>error</div>;
 
+        const { recipient, messages, currentUser } = recipientWithMessengerMessages.data;
+
         return (
-          <Scroll>
-            <div>{recipientWithMessengerMessages.data.recipient.name}</div>
-            {recipientWithMessengerMessages.data.messages.map((x) => (
-              <div key={x.id}>{x.text}</div>
-            ))}
-          </Scroll>
+          <>
+            <Scroll className="flex flex-col gap-3 p-6">
+              {messages.map((x) => (
+                <ChatMessage
+                  key={x.id}
+                  message={x.text}
+                  user={x.senderId === currentUser.id ? currentUser : recipient}
+                  isOwnMessage={x.senderId === currentUser.id}
+                />
+              ))}
+            </Scroll>
+            <MessengerMessageTextarea
+              value={text}
+              onInput={(x) => setText(x)}
+              disabled={isLoading}
+              onSubmit={async () => {
+                if (isLoading) return;
+
+                setIsLoading(true);
+                const resp = await createMessengerMessageRecord({
+                  pb,
+                  data: {
+                    text,
+                    senderId: recipientWithMessengerMessages.data.currentUser.id,
+                    recipientId: recipientWithMessengerMessages.data.recipient.id,
+                  },
+                });
+                setIsLoading(false);
+
+                if (resp.success) setText("");
+              }}
+            />
+          </>
         );
       })()}
-
-      <Textarea placeholder="Type something here" />
     </MainFixedLayout>
   );
 };
