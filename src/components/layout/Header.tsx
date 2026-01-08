59be0b9924b4b7819a3b72ef1c2d46a84bc2159d
robmolloy <robmolloy@hotmail.co.uk>
2026-01-08 22:17:11 +0000

    minor ui & ux fixes for logout (only shown when logged in)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,8 +4,10 @@ import { ThemeToggle } from "../ThemeToggle";
 import { Button } from "../ui/button";
 import { logout } from "@/modules/auth/dbAuthUtils";
 import { pb } from "@/config/pocketbaseConfig";
+import { useCurrentUserStore } from "@/stores/authDataStore";
 
 export function Header() {
+  const currentUserStore = useCurrentUserStore();
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-14 flex-1 items-center justify-between px-6">
@@ -15,10 +17,12 @@ export function Header() {
         </Link>
 
         <nav className="flex items-center space-x-2">
-          <Button variant="outline" onClick={() => logout({ pb })}>
-            <CustomIcon iconName="LogOut" size="md" />
-            Log Out
-          </Button>
+          {currentUserStore.data.authStatus === "loggedIn" && (
+            <Button variant="outline" onClick={() => logout({ pb })}>
+              <CustomIcon iconName="LogOut" size="md" />
+              Log Out
+            </Button>
+          )}
           <ThemeToggle />
         </nav>
       </div>
